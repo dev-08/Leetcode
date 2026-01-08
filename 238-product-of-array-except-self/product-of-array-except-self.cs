2499143b54ae135b9ad8622bcfc2421a8c47d32f
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-07 21:41:05 -0500

    Time: 1 ms (100.00%) | Memory: 66.7 MB (75.90%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cs
+++ 238-product-of-array-except-self/product-of-array-except-self.cs
@@ -2,8 +2,6 @@ public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
               int rp = 1;
             int [] left = new int[nums.Length];
-
-            int [] right = new int[nums.Length];
             for(int i =0; i<nums.Length;i++){
                     if(i==0){
                         left[i] = rp;
@@ -15,18 +13,11 @@ public class Solution {
             }
 
             rp = 1;
-            for(int j=nums.Length-1;j>=0 ; j--){
-                if(j==nums.Length-1) 
-                { right[j] = rp;}
-                else {
-                    rp = rp*nums[j+1]; 
-                    right[j] = rp; }
+            for(int i = nums.Length-2;i>=0;i--){
+                rp = rp*nums[i+1];
+                left[i] = left[i]*rp;
             }
 
-        for(int k= 0 ; k<nums.Length; k++){
-            nums[k] = left[k]*right[k];
-        }
-
-        return nums;
+        return left;
     }
 }
