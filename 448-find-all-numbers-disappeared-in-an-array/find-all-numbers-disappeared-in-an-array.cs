034edcba420b67119c9a14f0a0d914d3a9978d34
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-09 18:07:01 -0500

    Time: 5 ms (67.63%) | Memory: 71.3 MB (68.70%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cs
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cs
@@ -6,15 +6,19 @@ public class Solution {
         int [] temp = new int[n];
          IList<int> result = new List<int>();
         for(int i = 0 ; i<nums.Length;i++){
-            int val = nums[i];
-            temp[val-1] = -1;
+            int val = Math.Abs(nums[i]);
+            if(nums[val-1] > 0 ){
+                nums[val-1] = nums[val-1]*(-1);
+            }
         }
 
-        for(int i=0;i<temp.Length;i++){
-           if(temp[i] != -1){
-            result.Add(i+1);
-           }
+         for(int i = 0 ; i<nums.Length;i++){
+            if(nums[i] >0){
+                result.Add(i+1);
+            }
         }
+
+       
     return result;
 
     }
