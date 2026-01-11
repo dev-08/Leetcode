e08afcad30c2a20fd0cff590730b80c3c34f1806
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-11 09:54:44 -0500

    Time: 167 ms (55.61%) | Memory: 55.2 MB (61.43%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cs
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cs
@@ -1,31 +1,16 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
         
-        int m = matrix.Length;
-        bool a = false;
-        for(int i=0;i<m;i++)
-        {
-             a = binarysearch(matrix[i],target);
-            if(a==true){
-            break;
-            }
-        }
-        return a;
-    }
-
-    public bool binarysearch(int[] arr, int target){
-            int s = 0;
-            int e = arr.Length-1;
-            while(s<=e){
-                int mid = s+(e-s) /2;
-
-                if(arr[mid] == target){
-                    return true;
-                }
-                else if(arr[mid]<target){
-                    s = mid+1;
+            int m = matrix.Length;
+            int n = matrix[0].Length;
+            int i = 0;
+            int j = n-1;
+            while(i<m && j>=0){
+                if(matrix[i][j] == target)return true;
+                 if(matrix[i][j] <target){
+                    i++;
                 }else{
-                    e = mid-1;
+                    j--;
                 }
             }
 
