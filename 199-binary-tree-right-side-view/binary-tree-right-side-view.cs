73bb23a4952d2a6bce23272a9719df78e376ce32
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-14 20:20:33 -0500

    Time: 0 ms (100.00%) | Memory: 47 MB (95.30%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cs
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cs
@@ -12,26 +12,47 @@
  * }
  */
 public class Solution {
-    public IList<int> RightSideView(TreeNode root) {
-        
+
        IList<int> result = new List<int>();  
+    public IList<int> RightSideView(TreeNode root) {
+
+
+
+
+
+
        if(root == null) return result;
-       Queue<TreeNode> q = new Queue<TreeNode>();
-       q.Enqueue(root); 
-
-       while(q.Any()){
-        int size = q.Count;
-        for(int i=0;i<size;i++){
-            TreeNode t = q.Dequeue();
-            if(i==size-1){
-                result.Add(t.val);
-            }
-            if(t.left!=null) q.Enqueue(t.left);
-            if(t.right!=null)q.Enqueue(t.right);
-        }
-       }
-
+            dfs(root,0);
        return result;
+    //    Queue<TreeNode> q = new Queue<TreeNode>();
+    //    q.Enqueue(root); 
 
+    //    while(q.Any()){
+    //     int size = q.Count;
+    //     for(int i=0;i<size;i++){
+    //         TreeNode t = q.Dequeue();
+    //         if(i==size-1){
+    //             result.Add(t.val);
+    //         }
+    //         if(t.left!=null) q.Enqueue(t.left);
+    //         if(t.right!=null)q.Enqueue(t.right);
+    //     }
+    //    }
+
+    //    return result;
+
+    }
+
+    public void dfs(TreeNode root,int lvl){
+        if(root == null) return ;
+        if(result.Count == lvl){
+            result.Add(root.val);
+        }else{
+            result[lvl] = root.val;
+        }
+
+        dfs(root.left,lvl+1);
+        dfs(root.right,lvl+1);
+        
     }
 }
