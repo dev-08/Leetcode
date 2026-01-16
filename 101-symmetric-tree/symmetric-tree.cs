2478185d74dac48b568e762e0ba7cd7abb7fdd4f
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-15 21:54:33 -0500

    Time: 0 ms (100.00%) | Memory: 44.7 MB (27.17%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.cs
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    bool flag = true;
+    public bool IsSymmetric(TreeNode root) {
+            dfs(root.left , root.right);
+
+            return flag;
+    }
+
+
+    public void dfs(TreeNode left, TreeNode right){
+            if(left == null && right == null) return;
+
+            if(left == null || right == null || left.val != right.val) { flag = false; return;}
+
+            dfs(left.left,right.right);
+            dfs(left.right,right.left);
+
+
+    }
+}
+
+
+
+
+// public class Solution {
+//     public bool IsSymmetric(TreeNode root) {
+//         Queue<TreeNode> q = new Queue<TreeNode>();
+
+//         if(root.left!=null) q.Enqueue(root.left);
+//         if(root.right!=null) q.Enqueue(root.right);
+
+//         while(q.Any()){
+//             TreeNode f = q.Dequeue();
+//             TreeNode s = q.Dequeue();
+//             if(f.val != s.val) return false;
+
+//       q.Enqueue(f.left); 
+//         q.Enqueue(s.right);
+//           q.Enqueue(f.right);
+//             q.Enqueue(s.left);
+//         }
+
+
+//         return true;
+//     }
+// }
