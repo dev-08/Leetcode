daebff9f603433c785279263cfc01f2ab7522894
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-19 09:33:29 -0500

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.03%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+
+  public IList<int> arr = new List<int>();
+    public IList<int> InorderTraversal(TreeNode root) {
+        
+        
+        bst(root);
+        return arr;
+        
+    }
+
+
+    public void bst(TreeNode root){
+         if(root == null) return;
+
+        InorderTraversal(root.left);
+        arr.Add(root.val);
+         InorderTraversal(root.right);
+    }
+}
