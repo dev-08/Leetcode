2013d17e96d303db66676eea35285101ee956b62
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-19 09:05:55 -0500

    Time: 103 ms (7.66%) | Memory: 47.7 MB (74.09%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cs
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cs
@@ -23,21 +23,43 @@ public class Node {
 
 public class Solution {
     public Node Connect(Node root) {
-        if (root == null) return null;
-        Queue<Node> q = new Queue<Node>();
-        q.Enqueue(root);
-        while(q.Any()){
-            int size = q.Count;
-            for(int i=0;i<size;i++){
-               Node n =  q.Dequeue();
-               if(i == size-1){
-               n.next = null;
-               }else
-{             n.next = q.Peek();}
-               if(n.left!=null)  q.Enqueue(n.left);
-               if(n.right!=null) q.Enqueue(n.right);
+        if(root == null) return null;
+    Node level = root;
+    while(level.left!=null){
+        Node curr = level;
+        while(curr!=null){
+            curr.left.next = curr.right;
+            if(curr.next!=null){
+                curr.right.next = curr.next.left;
             }
+            curr = curr.next;
         }
-        return root;
+        level = level.left;
+    }
+
+    return root;
     }
 }
+
+
+
+// public class Solution {
+//     public Node Connect(Node root) {
+//         if (root == null) return null;
+//         Queue<Node> q = new Queue<Node>();
+//         q.Enqueue(root);
+//         while(q.Any()){
+//             int size = q.Count;
+//             for(int i=0;i<size;i++){
+//                Node n =  q.Dequeue();
+//                if(i == size-1){
+//                n.next = null;
+//                }else
+// {             n.next = q.Peek();}
+//                if(n.left!=null)  q.Enqueue(n.left);
+//                if(n.right!=null) q.Enqueue(n.right);
+//             }
+//         }
+//         return root;
+//     }
+// }
