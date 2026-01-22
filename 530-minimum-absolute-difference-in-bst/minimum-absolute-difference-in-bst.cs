71e74c201ff1b7ee0782317e97080dad10b304bd
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-22 18:19:48 -0500

    Time: 2 ms (18.27%) | Memory: 46.3 MB (50.25%) - LeetSync

---
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cs
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    IList<int> result = new List<int>();
+    public int GetMinimumDifference(TreeNode root) {
+        dfs(root);
+        int min = int.MaxValue;
+        int n = result.Count;
+        for(int i=0;i<n-1 ; i++){
+            int diff = Math.Abs(result[i] - result[i+1]);
+                min = Math.Min(diff,min);
+        }
+        
+        return min;
+    }
+
+
+    public void dfs(TreeNode root){
+        if(root == null) return;
+        dfs(root.left);
+        result.Add(root.val);
+        dfs(root.right);
+    }
+}
