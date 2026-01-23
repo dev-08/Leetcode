5958814e4ae45a65eaa5f44bc82da271eaca8ae8
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-23 15:29:37 -0500

    Time: 15 ms (99.34%) | Memory: 28.8 MB (73.37%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cs
+++ 1013-fibonacci-number/fibonacci-number.cs
@@ -1,8 +1,21 @@
 public class Solution {
+
+    int [] arr;
     public int Fib(int n) {
+        arr = new int[n+1];
+  
+        Array.Fill(arr, -1);
+        return fibona(n);
+
+    }
+
+    public int fibona(int n){
+        if(n==1 || n==0 ) return n;
         
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return Fib(n-2) + Fib(n-1);
+        if(arr[n]!=-1) return arr[n];
+
+        arr[n] = fibona(n-1) + fibona(n-2);
+        return arr[n];
+
     }
 }
