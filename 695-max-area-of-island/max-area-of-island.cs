2d762fda862dc632d5480f7b753eac108d5b5080
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-01-28 20:34:47 -0500

    Time: 5 ms (53.57%) | Memory: 46.5 MB (66.56%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cs
+++ 695-max-area-of-island/max-area-of-island.cs
@@ -1,9 +1,9 @@
 public class Solution {
-    int max = 0;
-    int count = 0;
+  
     public int MaxAreaOfIsland(int[][] grid) {
         int m = grid.Length;
         int n = grid[0].Length;
+        int max = 0;
          int[][] dirs = new int[][]{
                     new int[] { 0, 1 },
                     new int[] { 0, -1 },
@@ -12,30 +12,30 @@ public class Solution {
                 }; 
         for(int i = 0;i<m;i++){
             for(int j = 0 ; j<n;j++){
-                count = 0;
-                    dfs(grid,dirs,i,j,m,n);
-            max = Math.Max(count,max);
+            
+                    
+            max = Math.Max(dfs(grid,dirs,i,j,m,n),max);
             }
         }
         return max;
     }
 
 
-    public void dfs(int [][] grid,int[][] dirs,int i,int j,int m,int n){
+    public int dfs(int [][] grid,int[][] dirs,int i,int j,int m,int n){
             if(grid[i][j] == 0){
-                return;
+                return 0;
             }
-            grid[i][j] = 2;
-            count++;
+            grid[i][j] = 0;
+     int val = 1;
             foreach(int [] dir in dirs){
                 int nr = dir[0] + i;
                 int nc = dir[1] + j;
               
                 if (nr >= 0 && nc >= 0 && nr < m && nc < n  && grid[nr][nc] == 1) {
-                        dfs(grid,dirs,nr,nc,m,n);
+                 val =    val +    dfs(grid,dirs,nr,nc,m,n);
                 } 
             }
 
-            
+         return val;
     }
 }
