6e63cfde95654d0ee94290f2c31876ad3d1396e0
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-02-15 11:09:28 -0500

    Time: 3 ms (32.65%) | Memory: 50.5 MB (76.53%) - LeetSync

---
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int[] LeftRightDifference(int[] nums) {
+        int n = nums.Length;
+        int [] ans  = new int[n];
+        for(int i =1;i<n;i++){
+            nums[i] += nums[i-1] ;
+        }
+        int left = 0;
+        int right = 0;
+        for(int i=0;i<n;i++){
+            if(i==0){
+                left = 0;
+            }else{
+                left = nums[i-1];
+            }
+
+            if(i==n-1){
+                right = 0;
+            }else{
+                right = nums[n-1]-nums[i];
+            }
+
+
+    ans[i] = Math.Abs(left-right);
+
+        }
+
+        return ans;
+    }
+}
