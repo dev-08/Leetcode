9a2327ddf352b4409ac07708a538f4bc8e36aafa
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-02-21 16:26:06 -0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (60.82%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cs
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.cs
@@ -1,31 +1,29 @@
 public class Solution {
     public int FindMiddleIndex(int[] nums) {
-        int n = nums.Length;
-        for(int i = 1;i<n;i++){
-            nums[i] += nums[i-1];
+        for (int i = 1 ; i<nums.Length ; i++){
+            nums[i] +=  nums[i-1];
         }
 
         int left = 0;
-        int right = n-1;
-        int RightVal = 0 ;
-    
-        for(int i=0;i<n;i++){
-            if(i == 0) {
-                left = 0;}
-            else {
-                left = nums[i-1];
+        int right  =0 ;
+        for(int i = 0;i<nums.Length;i++){
+            if(i==0){
+                left = 0;
+            }else{
+ left = nums[i-1];
+
+            } 
+            
+            
+               
+                right = nums[nums.Length-1] - nums[i];
+            
+
+            if(left == right){
+                return i;
             }
 
-            if(i == right){
-                 RightVal = 0; }
 
-            else { 
-                RightVal = nums[right] - nums[i];
-            }
-
-
-
-            if(left==RightVal){return i;}
         }
 
         return -1;
