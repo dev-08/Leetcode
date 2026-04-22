a76845cfd5880df085098e6a1ecd181c39f510fd
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-04-21 20:55:17 -0400

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.11%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+       stringstream ss(s);
+       string word, lastword;
+
+       while(ss >> word){
+        lastword = word;
+       }
+
+        return lastword.length();
+    }
+};
