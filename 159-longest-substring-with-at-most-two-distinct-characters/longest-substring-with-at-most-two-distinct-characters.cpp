3900583201d83f9e6a172477cb3d21b8a0d57cef
Dev Patel <54015821+dev-08@users.noreply.github.com>
2026-05-19 20:53:16 -0400

    Time: 135 ms (24.69%) | Memory: 37.1 MB (58.58%) - LeetSync

---
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+       int n = s.size();
+       int l = 0;
+       int maxval = 0; 
+        unordered_map<char,int> map;
+
+        for(int r = 0 ;r<n;r++){
+            if(map.find(s[r])!=map.end()){
+                map[s[r]]++;
+            }else{
+                map[s[r]] = 1;
+            }
+
+
+            while(map.size()>2){
+                map[s[l]]--;
+                if(map[s[l]] < 1){
+                    map.erase(s[l]);
+                }
+                l++;
+            }
+
+            maxval = max(maxval,(r-l)+1);
+        }
+
+        return maxval;
+    }
+};
